1c00bfe65a6d93b57a2a6cb5766efe0da9c33d30
blexnmirian <blexnmirian@gmail.com>
2020-06-05 12:39:50 +0100

    second commit

--- public/app.js
+++ public/app.js
@@ -3,16 +3,108 @@ const url = "https://covidnigeria.herokuapp.com/api";
 fetch(url)
   .then(response => response.json())
   .then((response) => {
-    console.log(response);
+  
     
     //Declare Required variables
     const nigeria = response.data;
-    const abia =  response.data.states[33];
-    const imo =  response.data.states[31];
-    const enugu =  response.data.states[27];
-    const ebonyi =  response.data.states[28];
-   
+    const states = response.data.states;
+    const section = document.querySelector('.states');
 
+    const filter = document.querySelector('#filter');
+    //Event listener for filter
+    filter.addEventListener('keyup', filterState);
+    // filter states
+    function filterState(e){
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.states-collection').forEach(function(eachState){
+      const item = eachState.firstChild.textContent;
+      if(item.toLowerCase().indexOf(text) != -1){
+        eachState.style.display = 'block';
+      }else{
+        eachState.style.display = 'none'
+      }
+    })
+    }
+    
+
+    states.forEach(eachState => {
+          // create elements
+        console.log(eachState)
+        const div = document.createElement('div');
+        const card = document.createElement('div');
+        const cardHeader = document.createElement('div');
+        const cardBody = document.createElement('div');
+        const cardColumn = document.createElement('div')
+        const cardTitle = document.createElement('h6');
+        const cardText = document.createElement('p');
+
+        const cardColumn2 = document.createElement('div');
+        const cardTitle2 = document.createElement('h6');
+        const cardText2 = document.createElement('p');
+
+        const cardColumn3 = document.createElement('div');
+        const cardTitle3 = document.createElement('h6');
+        const cardText3 = document.createElement('p');
+
+
+
+        // add classes
+
+        div.className = 'col-sm-6 states-collection';
+        card.className = 'card mb-3 text-white bg-info';
+        cardHeader.className = 'card-header  text-center';
+        cardBody.className = 'card-body row text-center mt-3';
+        cardTitle.className ='card-title';
+        cardText.className = 'card-text btn btn-outline-warning';
+        cardColumn.className = ' col-sm-4 mt-3';
+        
+        cardColumn2.className = 'col-sm-4 mt-3';
+        cardTitle2.className = 'card-title';
+        cardText2.className = 'card-text btn btn-outline-dark';
+
+        cardColumn3.className = 'col-sm-4 mt-3';
+        cardTitle3.className = 'card-title';
+        cardText3.className = 'card-text btn btn-outline-danger'
+
+
+
+
+        
+        //fetch data
+        cardTitle.textContent = "Confirmed Cases";
+        cardTitle2.textContent = "Discharged";
+        cardTitle3.textContent = "Death";
+        cardHeader.appendChild(document.createTextNode(eachState.state));
+        cardText.appendChild(document.createTextNode(eachState.confirmedCases));
+        cardText2.appendChild(document.createTextNode(eachState.discharged));
+        cardText3.appendChild(document.createTextNode(eachState.death));
+
+
+
+        //Append  elements
+
+        cardColumn.appendChild(cardTitle);
+        cardColumn.appendChild(cardText);
+
+        cardColumn2.appendChild(cardTitle2);
+        cardColumn2.appendChild(cardText2);
+
+        cardColumn3.appendChild(cardTitle3);
+        cardColumn3.appendChild(cardText3);
+
+        cardBody.appendChild(cardColumn);
+        cardBody.appendChild(cardColumn2);
+        cardBody.appendChild(cardColumn3);
+
+        card.appendChild(cardHeader);
+        card.appendChild(cardBody);
+
+        div.appendChild(card);
+
+        section.appendChild(div);
+      
+      });
+    
     // get  our ui elements
 
     const el_nigeria = document.querySelector('.nigeria');
@@ -23,56 +115,8 @@ fetch(url)
 
 
 
-    const el_abia = document.querySelector('.abia');
-    const el_abiaReport = document.querySelector('.abia__report');
-    const el_abiaDischarge = document.querySelector('.abia__discharge');
-    const el_abiaDeath = document.querySelector('.abia__death')
-
-    const el_imo = document.querySelector('.imo');
-    const el_imoReport = document.querySelector('.imo__report');
-    const el_imoDischarge = document.querySelector('.imo__discharge');
-    const el_imoDeath = document.querySelector('.imo__death')
-
-    // const el_lagos = document.querySelector('.lagos');
-    // const el_lagosReport = document.querySelector('.lagos__report');
-
-    const el_enugu = document.querySelector('.enugu');
-    const el_enuguReport = document.querySelector('.enugu__report');
-    const el_enuguDischarge = document.querySelector('.enugu__discharge');
-    const el_enuguDeath = document.querySelector('.enugu__death')
-
-
-    const el_ebonyi = document.querySelector('.ebonyi');
-    const el_ebonyiReport = document.querySelector('.ebonyi__report');
-    const el_ebonyiDischarge = document.querySelector('.ebonyi__discharge');
-    const el_ebonyiDeath = document.querySelector('.ebonyi__death')
-
-    //append data from api
-    el_abia.textContent = abia.state;
-    el_abiaReport.textContent = abia.confirmedCases;
-    el_abiaDischarge.textContent = abia.discharged;
-    el_abiaDeath.textContent = abia.death;
-    
-
-    el_imo.textContent = imo.state;
-    el_imoReport.textContent = imo.confirmedCases;
-    el_imoDischarge.textContent = imo.discharged;
-    el_imoDeath.textContent = imo.death;
-
-    el_enugu.textContent = enugu.state;
-    el_enuguReport.textContent = enugu.confirmedCases;
-    el_enuguDischarge.textContent = imo.discharged;
-    el_enuguDeath.textContent = imo.death;
-
-
-//    el_lagos.textContent = lagos.state;
-//    el_lagosReport.textContent = lagos.confirmedCases;
    
-   el_ebonyi.textContent = ebonyi.state;
-   el_ebonyiReport.textContent = ebonyi.confirmedCases;
-   el_ebonyiDischarge.textContent = ebonyi.discharged;
-   el_ebonyiDeath.textContent = ebonyi.death;
-
+    
 
    el_nigeria.textContent = "Nigeria";
    el_nigeriaConfirmedCases.textContent = nigeria.totalConfirmedCases;
